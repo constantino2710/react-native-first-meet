7ce9df51df8fba497ed53563d6584ffaa5e6e354
constantino2710 <constantinodev27@gmail.com>
2024-08-14 12:32:05 -0300

    atualização do app.tsx

--- App.tsx
+++ App.tsx
@@ -1,33 +1,6 @@
-import { Text, View, StyleSheet } from 'react-native'; 
+import {Home} from "./src/screens/home";
 export default function App(){
   return(
-    <View style = {styles.container}>
-
-    <Text style = {styles.eventName}>
-      Nome do evento
-      </Text>
-
-    <Text style = {styles.eventDate}>
-      sabado 8 de outubro de 2024
-      </Text>
-    </View>
+    <Home/>
   )
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-      backgroundColor: 'black',
-      padding: 24,
-  },
-  eventName: {
-      color: 'white',
-      fontWeight: 'bold',
-      marginTop: 48,
-      fontSize: 24,
-  },
-  eventDate: {
-    color: 'gray',
-    fontSize: 16,
-  }
-});
