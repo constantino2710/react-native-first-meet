c8884e05ed418f17a0d0daaabd92a7c6d41b34a1
constantino2710 <constantinodev27@gmail.com>
2024-08-14 11:59:19 -0300

    app.tsx update

--- App.tsx
+++ App.tsx
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native'; 
+export default function App(){
+  return(
+    <View style = {styles.container}>
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <Text style = {styles.eventName}>
+      Nome do evento
+      </Text>
+
+    <Text style = {styles.eventDate}>
+      sabado 8 de outubro de 2024
+      </Text>
     </View>
-  );
-}
+  )
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+      backgroundColor: 'black',
+      padding: 24,
   },
+  eventName: {
+      color: 'white',
+      fontWeight: 'bold',
+      marginTop: 48,
+      fontSize: 24,
+  },
+  eventDate: {
+    color: 'gray',
+    fontSize: 16,
+  }
 });
